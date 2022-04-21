21cdbf228afc42a684074fff44af3d406abd5369
GuestJim <30958420+GuestJim@users.noreply.github.com>
2022-04-21 19:04:18 -0400

    Correct Transparency to Opacity
    For how the slider actually works, Opacity is the correct term

--- app_UI.r
+++ app_UI.r
@@ -278,9 +278,9 @@ GraphTabUI	<-	function(id, showGRAPHS = TRUE, BRUSH = TRUE, ..., label = "Graphs
 					actionButton(inputId	=	"brushDIFFupdate", label = "Update Single Plot"),
 					fixedRow(
 						column(3, checkboxInput(inputId	=	"diffLimHeat",	label	=	"Enable Heat Map",	value	=	TRUE)	),
-						column(5, sliderInput(inputId	=	"brushDIFFalpha", label = "Heatmap Transparency",
+						column(5, sliderInput(inputId	=	"brushDIFFalpha", label = "Heatmap Opacity",
 							min = 0,	max = 1,	value = 1,	step = 0.1)	),
-						column(3, actionButton(inputId	=	"brushDIFFalphup", label = "Apply Transparency")	),
+						column(3, actionButton(inputId	=	"brushDIFFalphup", label = "Apply Opacity")	),
 					),
 					plotOutput('brushDIFFfacet',	height=720)	)
 				),
